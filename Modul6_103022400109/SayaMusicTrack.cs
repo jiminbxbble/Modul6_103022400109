2857a12b050fd4887ebc8f7001f80580c1b419de
Nopalky <novalguci20@gmail.com>
2026-04-08 07:56:07 +0700

    menambahkan isi kelas
    Add basic music user and track management classes
    
    Replaced default Program.cs with a new structure under Modul6_103022400109. Added SayaMusicTrack and SayaMusicUser classes to represent music tracks and users, including methods for track details, play count management, and user track aggregation. The main program demonstrates creating a user and multiple tracks, and printing their details.

---
+++ Modul6_103022400109/SayaMusicTrack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Modul6_103022400109
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        public string title;
+        private int playCount;
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+            this.playCount = 0;
+
+            Random rnd = new Random();
+            this.id = rnd.Next(10000, 999999);
+        }
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"ID Lagu		: {this.id}");
+            Console.WriteLine($"Judul Lagu	: {this.title}");
+            Console.WriteLine($"Total Play	: {this.playCount}");
+            Console.WriteLine("---------------------------------------");
+
+        }
+        public int GetPlayCount()
+        {
+            return playCount;
+        }
+    }
+}
+
---
+++ Modul6_103022400109/SayaMusicUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Modul6_103022400109
+{
+    public class SayaMusicUser
+    {
+        private int id;
+        public string username;
+        private List<SayaMusicTrack> uploadedTracks;
+        public SayaMusicUser(string username)
+        {
+            uploadedTracks = new List<SayaMusicTrack>();
+            this.username = username;
+        }
+        public int GetTotalPlayCount()
+        {
+            int totalPlayCount = 0;
+            foreach (var track in uploadedTracks)
+            {
+                totalPlayCount += track.GetPlayCount();
+            }
+            return totalPlayCount;
+        }
+        public void AddTrack(SayaMusicTrack track)
+        {
+            if (track == null)
+                if (track.GetPlayCount() == int.MaxValue)
+                    uploadedTracks.Add(track);
+        }
+        public void PrintAllTrack()
+        {
+            foreach (var track in uploadedTracks)
+            {
+                track.PrintTrackDetails();
+            }
+        }
+
+    }
+}
